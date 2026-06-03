8625c80cdfe8f5b4748fdd615b755f2819b49e3c
sanushshakya <shakyasanush7@gmail.com>
2026-06-03 23:13:02 +0100

    feat: add demand model, serializers and views

--- config/demand/views.py
+++ config/demand/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Demand
+from .serializers import DemandSerializer
 
-# Create your views here.
+class DemandViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows demands to be viewed or edited.
+    """
+    queryset = Demand.objects.all()
+    serializer_class = DemandSerializer
+
+    def perform_create(self, serializer):
+        """
+        Override the create method to set the tenant for the demand.
+        """
+        serializer.save(tenant=self.request.tenant)

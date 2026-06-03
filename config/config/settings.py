2c379674606646216584ff006b6c174b01d9f883
sanushshakya <shakyasanush7@gmail.com>
2026-06-03 23:07:54 +0100

    feat: scaffold inventory app and register in settings

--- config/config/settings.py
+++ config/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "inventory",  # Added the 'inventory' app to INSTALLED_APPS
 ]
 
 MIDDLEWARE = [
@@ -115,8 +116,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"

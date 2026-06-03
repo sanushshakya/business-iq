66287f4f713d7b4c5bc28703a6f76b01bac5bc00
sanushshakya <shakyasanush7@gmail.com>
2026-06-03 22:32:55 +0100

    remove files

--- config/demand/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-config/demand/__init__.py
-
-This module is an empty file to indicate that 'demand' is a Django application.
-"""
-
-# No additional code needed for this empty initialization file
--- config/logistics/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-config/logistics/__init__.py
-
-This file is intentionally left blank to mark the directory as a Python package.
-"""
-
-# No additional code is required for an empty __init__.py file.
-# It can be used to specify attributes like __package__, but it's not necessary here.

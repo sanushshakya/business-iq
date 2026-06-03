db8eeef9f2ef2294b36522d1d27b1e14e418181b
sanushshakya <shakyasanush7@gmail.com>
2026-06-03 23:09:19 +0100

    feat: add inventory model, serializers and views

--- config/inventory/views.py
+++ config/inventory/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# inventory/views.py
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from .models import InventoryItem
+from .serializers import InventoryItemSerializer
+
+class InventoryItemViewSet(ModelViewSet):
+    """
+    API endpoint that allows inventory items to be viewed or edited.
+    """
+
+    queryset = InventoryItem.objects.all()
+    serializer_class = InventoryItemSerializer

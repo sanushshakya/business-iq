9ab19c7bd916145080937016ea7864926b3c74f1
sanushshakya <shakyasanush7@gmail.com>
2026-06-03 22:31:44 +0100

    feat(config): add demand app

---
+++ config/demand/__init__.py
@@ -0,0 +1,7 @@
+"""
+config/demand/__init__.py
+
+This module is an empty file to indicate that 'demand' is a Django application.
+"""
+
+# No additional code needed for this empty initialization file

b72e56af3e4eb1713de68aa71cac8389dc507bb0
sanushshakya <shakyasanush7@gmail.com>
2026-06-04 20:08:15 +0100

    feat: scaffold pricing app and register in settings

--- config/config/settings.py
+++ config/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "inventory",  # Added the 'inventory' app to INSTALLED_APPS
     "demand",  # Added the 'demand' app to INSTALLED_APPS
     "logistics",  # Added the 'logistics' app to INSTALLED_APPS
+    "pricing",  # Added the 'pricing' app to INSTALLED_APPS
 ]
 
 MIDDLEWARE = [
@@ -105,13 +106,19 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
-# 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

0a06bfcc1a9f5c132c4291769ea412b4e7c66e69
sanushshakya <shakyasanush7@gmail.com>
2026-06-04 20:09:55 +0100

    feat: add pricing model, serializers and views

--- config/pricing/views.py
+++ config/pricing/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+"""
+config/pricing/views.py
 
-# Create your views here.
+This file contains Django REST Framework (DRF) ViewSets for the pricing app.
+"""
+
+from rest_framework import viewsets
+from .models import PricePlan
+from .serializers import PricePlanSerializer
+
+
+class PricePlanViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows price plans to be viewed or edited.
+    """
+
+    queryset = PricePlan.objects.all()
+    serializer_class = PricePlanSerializer
+
+    def perform_create(self, serializer):
+        """
+        Custom method to create a new price plan and assign the requesting user as its creator.
+
+        :param serializer: Serializer instance containing validated data
+        """
+        serializer.save(created_by=self.request.user)
+
+    def perform_update(self, serializer):
+        """
+        Custom method to update an existing price plan and record the last updated user.
+
+        :param serializer: Serializer instance containing validated data
+        """
+        serializer.save(last_updated_by=self.request.user)

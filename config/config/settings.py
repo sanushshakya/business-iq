a4d8486a97fc48f8ec57962f1a743373488f37cb
sanushshakya <shakyasanush7@gmail.com>
2026-06-04 20:12:42 +0100

    feat: scaffold sync app and register in settings

--- config/config/settings.py
+++ config/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "demand",  # Added the 'demand' app to INSTALLED_APPS
     "logistics",  # Added the 'logistics' app to INSTALLED_APPS
     "pricing",  # Added the 'pricing' app to INSTALLED_APPS
+    "sync",  # Added the 'sync' app to INSTALLED_APPS
 ]
 
 MIDDLEWARE = [
@@ -106,19 +107,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = "static/"

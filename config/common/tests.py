3541593658c58b7e0f8b26a00d15fb8669215786
sanushshakya <shakyasanush7@gmail.com>
2026-06-05 22:11:18 +0100

    feat(config/common): add basic tests.py

---
+++ config/common/tests.py
@@ -0,0 +1,22 @@
+"""
+config/common/tests.py
+
+This is a basic test file for the 'config.common' app in the iq project.
+It includes a placeholder test case to ensure the testing framework works as expected.
+"""
+
+import unittest
+
+class TestPlaceholder(unittest.TestCase):
+    """
+    A placeholder test case to demonstrate how tests should be structured.
+    """
+
+    def test_example(self):
+        """
+        An example test method that always passes.
+        """
+        self.assertEqual(1, 1)  # This will pass because 1 is indeed equal to 1
+
+if __name__ == '__main__':
+    unittest.main()

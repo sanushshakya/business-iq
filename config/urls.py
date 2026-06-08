f5383feee44c17b9446c11c9ff5a15d2e916e7e4
sanushshakya <shakyasanush7@gmail.com>
2026-06-08 14:45:34 +0100

    feat(config): add /api/docs/ URL pattern

--- config/urls.py
+++ config/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    
+    # New URL pattern for API documentation
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='api-docs'),
 ]

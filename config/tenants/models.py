f645643207add8d14b021586798c9453379e1bb1
sanushshakya <shakyasanush7@gmail.com>
2026-06-08 15:24:00 +0100

    feat(tenants): add Till model

--- config/tenants/models.py
+++ config/tenants/models.py
@@ -58,11 +58,13 @@ class Till(models.Model):
     Fields:
     branch (ForeignKey): The branch where this till is located.
     number (int): The number of the till.
+    device_id (str): The unique identifier for the till device.
     is_active (bool): Whether the till is active or not.
     created_at (datetime): The timestamp when the till was created.
     """
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='tills', verbose_name='Branch')
     number = models.IntegerField(verbose_name='Till Number')
+    device_id = models.CharField(max_length=100, unique=True, verbose_name='Device ID')
     is_active = models.BooleanField(default=True, verbose_name='Is Active')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created At')
 
@@ -87,54 +89,4 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Branch',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255, verbose_name='Branch Name')),
-                ('address', models.TextField(verbose_name='Address')),
-                ('is_active', models.BooleanField(default=True, verbose_name='Is Active')),
-                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Created At')),
-                ('company', models.ForeignKey(on_delete=models.CASCADE, related_name='branches', to='tenants.company', verbose_name='Company')),
-            ],
-            options={
-                'verbose_name': 'Branch',
-                'verbose_name_plural': 'Branches',
-            },
-        ),
-    ]
-
-
-# Migration for the Till model
-from django.db import migrations, models
-
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ('tenants', '0002_branch'),
-    ]
-
-    operations = [
-        migrations.CreateModel(
-            name='Till',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('number', models.IntegerField(verbose_name='Till Number')),
-                ('is_active', models.BooleanField(default=True, verbose_name='Is Active')),
-                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Created At')),
-                ('branch', models.ForeignKey(on_delete=models.CASCADE, related_name='tills', to='tenants.branch', verbose_name='Branch')),
-            ],
-            options={
-                'verbose_name': 'Till',
-                'verbose_name_plural': 'Tills',
-            },
-        ),
-    ]
-
-
-# Registering the models in Django admin
-from django.contrib import admin
-
-# Register your models here.
-from .models import Company, Branch, Till
-
-admin.site.register(Company)
-admin.site.register(Branch)
-admin.site.register(Till)
+                ('id', models.AutoField(auto_created=True

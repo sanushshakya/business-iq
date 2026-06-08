7d9494f2902ee5ad5d95886b457c8fb1544f2281
sanushshakya <shakyasanush7@gmail.com>
2026-06-08 15:24:28 +0100

    feat(tenants): register models in Django admin

--- config/tenants/admin.py
+++ config/tenants/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+# Import models from tenants app
+from .models import Company, Branch, Till
+
+# Register models in Django admin with list_display showing parent relationships
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Company model.
+    """
+    list_display = ('name', 'code')
+
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Branch model.
+    """
+    list_display = ('name', 'company', 'location')
+
+@admin.register(Till)
+class TillAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Till model.
+    """
+    list_display = ('branch', 'number')

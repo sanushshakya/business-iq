abb4080e33e3742b64df81d19a431b7b0908fc3f
sanushshakya <shakyasanush7@gmail.com>
2026-06-08 14:53:32 +0100

    refactor: update Meta classes to be abstract=True for new models

--- config/common/models.py
+++ config/common/models.py
@@ -14,3 +14,16 @@ class BranchScopedModel(TenantScopedModel):
 
     class Meta:
         abstract = True
+
+# Add TenantScopedModel here if it's not already defined in apps/common/models.py
+class TenantScopedModel(models.Model):
+    """
+    Abstract base model for models that need to be scoped by tenant.
+    
+    Attributes:
+        tenant (Tenant): A foreign key to the Tenant model, representing the tenant scope.
+    """
+    tenant = models.ForeignKey('apps.common.models.Tenant', on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True

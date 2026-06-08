dd51726cc23fb1dd4ad10015268a6d0297ab4b9c
sanushshakya <shakyasanush7@gmail.com>
2026-06-08 14:57:23 +0100

    chore: add BaseModel with id, created_at, updated_at to config/config/common/models.py

--- config/common/models.py
+++ config/common/models.py
@@ -1,21 +1,8 @@
 # config/common/models.py
 
 from django.db import models
-from apps.common.models import TenantScopedModel, StoreBranch
 
-class BranchScopedModel(TenantScopedModel):
-    """
-    Abstract model that extends TenantScopedModel and includes a foreign key to the StoreBranch model.
-    
-    Attributes:
-        branch (StoreBranch): A foreign key to the StoreBranch model, representing the branch scope.
-    """
-    branch = models.ForeignKey('apps.common.models.StoreBranch', on_delete=models.CASCADE)
 
-    class Meta:
-        abstract = True
-
-# Add TenantScopedModel here if it's not already defined in apps/common/models.py
 class TenantScopedModel(models.Model):
     """
     Abstract base model for models that need to be scoped by tenant.
@@ -24,6 +11,18 @@ class TenantScopedModel(models.Model):
         tenant (Tenant): A foreign key to the Tenant model, representing the tenant scope.
     """
     tenant = models.ForeignKey('apps.common.models.Tenant', on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+        
+class BranchScopedModel(TenantScopedModel):
+    """
+    Abstract model that extends TenantScopedModel and includes a foreign key to the StoreBranch model.
+    
+    Attributes:
+        branch (StoreBranch): A foreign key to the StoreBranch model, representing the branch scope.
+    """
+    branch = models.ForeignKey('apps.common.models.StoreBranch', on_delete=models.CASCADE)
 
     class Meta:
         abstract = True

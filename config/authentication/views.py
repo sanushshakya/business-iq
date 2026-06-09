d048ee2ea0fff68b50fd0e3aa7580e0af7db292a
sanushshakya <shakyasanush7@gmail.com>
2026-06-09 21:17:09 +0100

    refactor(authentication): add TenantModelViewSet base class for multi-tenant filtering

--- config/authentication/views.py
+++ config/authentication/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
+from django.db.models import Q
 
-# Create your views here.
+class TenantModelViewSet(viewsets.ModelViewSet):
+    """
+    Base ViewSet class for handling multi-tenant models.
+
+    This class overrides the `get_queryset` method to filter objects by the current company.
+    """
+
+    def get_queryset(self):
+        """
+        Override the default queryset to filter by the request's associated company.
+
+        Returns:
+            QuerySet: Filtered queryset based on the request's company.
+        """
+        user = self.request.user
+        if hasattr(user, 'company'):
+            return self.queryset.filter(Q(company=user.company))
+        else:
+            return self.queryset.none()

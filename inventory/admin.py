ec4ed625d7be9d4fa6613e7bce6e327261a87ee4
sanushshakya <shakyasanush7@gmail.com>
2026-06-10 20:33:29 +0100

    chore(inventory): register StockBatch and StockMovement in admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import StockBatch, StockMovement
+
+@admin.register(StockBatch)
+class StockBatchAdmin(admin.ModelAdmin):
+    """
+    Admin class for the StockBatch model.
+    """
+
+    list_display = ('batch_id', 'product_name', 'quantity', 'expiry_date')
+    search_fields = ['batch_id', 'product_name']
+    list_filter = ['expiry_date']
+
+@admin.register(StockMovement)
+class StockMovementAdmin(admin.ModelAdmin):
+    """
+    Admin class for the StockMovement model.
+    """
+
+    list_display = ('movement_id', 'batch', 'type', 'quantity', 'timestamp')
+    search_fields = ['movement_id', 'type']
+    list_filter = ['type', 'timestamp']

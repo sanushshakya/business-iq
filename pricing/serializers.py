3d33251c384a0bb0077a2de53aad46e68bbb0a44
sanushshakya <shakyasanush7@gmail.com>
2026-06-15 23:17:54 +0100

    feat(pricing): add InvoiceLineItem serializer

--- pricing/serializers.py
+++ pricing/serializers.py
@@ -1,12 +1,12 @@
 # serializers.py
 
 from rest_framework import serializers
-from .models import PricingPlan, SupplierInvoice
+from .models import PricingPlan, SupplierInvoice, InvoiceLineItem
 
 class PricingPlanSerializer(serializers.ModelSerializer):
     """
     Serializer for PricingPlan model.
-    
+
     This serializer is used to serialize and deserialize PricingPlan objects
     in the RESTful API. It includes all fields from the PricingPlan model.
     """
@@ -23,7 +23,7 @@ class PricingPlanSerializer(serializers.ModelSerializer):
 class SupplierInvoiceSerializer(serializers.ModelSerializer):
     """
     Serializer for SupplierInvoice model.
-    
+
     This serializer is used to serialize and deserialize SupplierInvoice objects
     in the RESTful API. It includes all fields from the SupplierInvoice model.
     """
@@ -35,4 +35,21 @@ class SupplierInvoiceSerializer(serializers.ModelSerializer):
         Defines the model and fields to be serialized.
         """
         model = SupplierInvoice
+        fields = '__all__'
+
+class InvoiceLineItemSerializer(serializers.ModelSerializer):
+    """
+    Serializer for InvoiceLineItem model.
+
+    This serializer is used to serialize and deserialize InvoiceLineItem objects
+    in the RESTful API. It includes all fields from the InvoiceLineItem model.
+    """
+    
+    class Meta:
+        """
+        Meta class for InvoiceLineItemSerializer.
+        
+        Defines the model and fields to be serialized.
+        """
+        model = InvoiceLineItem
         fields = '__all__'

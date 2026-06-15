86102fc5f28f20cc5d305a90d27a7c5f3bd7495b
sanushshakya <shakyasanush7@gmail.com>
2026-06-15 23:17:17 +0100

    feat(pricing): add SupplierInvoice serializer

--- pricing/serializers.py
+++ pricing/serializers.py
@@ -1,7 +1,7 @@
 # serializers.py
 
 from rest_framework import serializers
-from .models import PricingPlan
+from .models import PricingPlan, SupplierInvoice
 
 class PricingPlanSerializer(serializers.ModelSerializer):
     """
@@ -18,4 +18,21 @@ class PricingPlanSerializer(serializers.ModelSerializer):
         Defines the model and fields to be serialized.
         """
         model = PricingPlan
+        fields = '__all__'
+
+class SupplierInvoiceSerializer(serializers.ModelSerializer):
+    """
+    Serializer for SupplierInvoice model.
+    
+    This serializer is used to serialize and deserialize SupplierInvoice objects
+    in the RESTful API. It includes all fields from the SupplierInvoice model.
+    """
+    
+    class Meta:
+        """
+        Meta class for SupplierInvoiceSerializer.
+        
+        Defines the model and fields to be serialized.
+        """
+        model = SupplierInvoice
         fields = '__all__'

25a86725256b4917c2356e6e13120bf416464a9a
sanushshakya <shakyasanush7@gmail.com>
2026-06-19 09:03:57 +0100

    chore: Set up routing for WebSocket consumer

--- config/asgi.py
+++ config/asgi.py
@@ -10,7 +10,20 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import common.routing  # Import the consumer routing from your app's routing file
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            common.routing.websocket_urlpatterns  # Use the imported routing
+        )
+    ),
+})
+```
+
+--- END ---

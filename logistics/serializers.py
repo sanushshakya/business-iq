03bbe1b04484c1df87c6b4fa25920fdec9621d5b
sanushshakya <shakyasanush7@gmail.com>
2026-06-20 10:00:23 +0100

    chore(logistics): add serializers for UserSupplier and AlternativeSupplier

--- logistics/serializers.py
+++ logistics/serializers.py
@@ -1,20 +1,47 @@
-"""
-serializers.py
-
-This file contains Django REST Framework (DRF) serializers for the logistics app.
-"""
+# logistics/serializers.py
 
 from rest_framework import serializers
-from .models import Logistic
+from .models import UserSupplier, AlternativeSupplier
 
-
-class LogisticSerializer(serializers.ModelSerializer):
+class UserSupplierSerializer(serializers.ModelSerializer):
     """
-    Serializer for the Logistic model.
-
-    This serializer converts Logistic model instances into JSON format and vice versa.
+    Serializer for converting UserSupplier model instances to JSON and vice versa.
+    
+    Fields:
+    - id: IntegerField representing the primary key of the user supplier.
+    - name: CharField representing the name of the user supplier.
+    - contact_email: EmailField representing the contact email of the user supplier.
+    - phone_number: CharField representing the phone number of the user supplier.
+    - address_line1: CharField representing the first line of the user supplier's address.
+    - address_line2: CharField representing the second line of the user supplier's address (optional).
+    - city: CharField representing the city of the user supplier's address.
+    - state: CharField representing the state of the user supplier's address.
+    - postal_code: CharField representing the postal code of the user supplier's address.
+    - country: CharField representing the country of the user supplier's address.
     """
 
     class Meta:
-        model = Logistic
-        fields = "__all__"
+        model = UserSupplier
+        fields = '__all__'
+
+
+class AlternativeSupplierSerializer(serializers.ModelSerializer):
+    """
+    Serializer for converting AlternativeSupplier model instances to JSON and vice versa.
+    
+    Fields:
+    - id: IntegerField representing the primary key of the alternative supplier.
+    - name: CharField representing the name of the alternative supplier.
+    - contact_email: EmailField representing the contact email of the alternative supplier.
+    - phone_number: CharField representing the phone number of the alternative supplier.
+    - address_line1: CharField representing the first line of the alternative supplier's address.
+    - address_line2: CharField representing the second line of the alternative supplier's address (optional).
+    - city: CharField representing the city of the alternative supplier's address.
+    - state: CharField representing the state of the alternative supplier's address.
+    - postal_code: CharField representing the postal code of the alternative supplier's address.
+    - country: CharField representing the country of the alternative supplier's address.
+    """
+
+    class Meta:
+        model = AlternativeSupplier
+        fields = '__all__'

154ed2a77a47c33d5adcce22ce10deba0814a21b
sanushshakya <shakyasanush7@gmail.com>
2026-06-20 10:01:41 +0100

    chore(logistics): add read-only view for AlternativeSupplier

--- logistics/views.py
+++ logistics/views.py
@@ -1,88 +1,20 @@
-# logistics/views.py
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import AlternativeSupplier
 
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from .models import UserSupplier, AlternativeSupplier, FreightAlert
-
-class UserSupplierCreateView(CreateView):
+class AlternativeSupplierListView(ListView):
     """
-    View for creating a new UserSupplier instance.
+    A read-only view for displaying a list of all AlternativeSuppliers.
 
-    Handles the creation form and saves the new instance to the database.
-    """
-    model = UserSupplier
-    fields = ['supplier_name', 'contact_info']
-    success_url = reverse_lazy('user_supplier_list')
+    This view inherits from Django's generic ListView and is designed to handle GET requests,
+    rendering a template with a queryset of all AlternativeSupplier objects.
+    
+    Template:
+    - 'logistics/alternative_supplier_list.html'
 
-    def form_valid(self, form):
-        """
-        Validate the form data and save the new UserSupplier instance.
-
-        :param form: Form containing the user supplier data.
-        :return: Redirect to list view on successful creation.
-        """
-        response = super().form_valid(form)
-        return response
-
-class UserSupplierUpdateView(UpdateView):
-    """
-    View for updating an existing UserSupplier instance.
-
-    Handles the update form and updates the instance in the database.
-    """
-    model = UserSupplier
-    fields = ['supplier_name', 'contact_info']
-    success_url = reverse_lazy('user_supplier_list')
-
-    def get_object(self):
-        """
-        Retrieve the UserSupplier object based on the URL parameter.
-
-        :return: UserSupplier instance matching the primary key.
-        """
-        return get_object_or_404(UserSupplier, pk=self.kwargs['pk'])
-
-class UserSupplierDeleteView(DeleteView):
-    """
-    View for deleting an existing UserSupplier instance.
-
-    Handles the deletion of a UserSupplier instance from the database.
-    """
-    model = UserSupplier
-    success_url = reverse_lazy('user_supplier_list')
-
-    def get_object(self):
-        """
-        Retrieve the UserSupplier object based on the URL parameter.
-
-        :return: UserSupplier instance matching the primary key.
-        """
-        return get_object_or_404(UserSupplier, pk=self.kwargs['pk'])
-
-def user_supplier_list(request):
-    """
-    View for listing all UserSupplier instances.
-
-    Displays a list of all UserSuppliers in the database.
-    """
-    usersuppliers = UserSupplier.objects.all()
-    context = {'usersuppliers': usersuppliers}
-    return render(request, 'logistics/user_supplier_list.html', context)
-
-class AlternativeSupplierReadView(DetailView):
-    """
-    View for reading a specific AlternativeSupplier instance.
-
-    Displays details of an AlternativeSupplier instance.
+    Context:
+    - 'alternative_suppliers': A list of AlternativeSupplier objects
     """
     model = AlternativeSupplier
-    template_name = 'logistics/alternative_supplier_detail.html'
-
-    def get_object(self):
-        """
-        Retrieve the AlternativeSupplier object based on the URL parameter.
-
-        :return: AlternativeSupplier instance matching the primary key.
-        """
-        return get_object_or_404(AlternativeSupplier, pk=self.kwargs['pk'])
+    template_name = 'logistics/alternative_supplier_list.html'
+    context_object_name = 'alternative_suppliers'
